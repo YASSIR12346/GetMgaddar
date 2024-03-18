77b5975e83ac588cf4a07281a4727ea2258909bc
Yassir Nasri <ynasri@smb.sqli.com>
2024-03-18 10:12:30 +0100

    latest

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { GoalService } from '../goal.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { Goal } from '../Models/Goal';
 
 @Component({
   selector: 'mg-goal',
   templateUrl: './goal.component.html',
   styleUrls: ['./goal.component.scss']
 })
-export class GoalComponent {
+export class GoalComponent implements OnInit{
+
+  private readonly goalService=inject(GoalService);
+  dataSource = new MatTableDataSource<Goal>();
+
+  ngOnInit(): void {
+    this.goalService.getAllGoals().subscribe((data) => {
+      this.dataSource.data = data as Goal[];
+      console.log(this.dataSource.data);
+    });
+  }
 
 }
